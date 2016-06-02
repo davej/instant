ad498f07f3a88686d0e103ec500c7cd0959ab3ef
DaveJ <dave@davejeffery.com>
2016-06-02 11:42:37 +0100

    click the banner to refresh the page

--- client/connectionLost.js
+++ client/connectionLost.js
@@ -5,9 +5,14 @@ module.exports = function connectionLost() {
   style += 'width: 100%; padding: 20px; text-align: center; border-bottom: 1px #555 solid;'
   style += 'z-index: 9999; text-shadow: 1px 1px 0px white;'
 
+  var innerHtml = 'Pingy has lost contact with this page. Live Reload won\'t work.<br />'
+  innerHtml += 'Click this banner to refresh the page.'
+
   var div = document.createElement('div')
   div.id = 'pingyConnectionLost'
-  div.innerHTML = 'Pingy has lost contact with this page. You will need to refresh the page to get Instant Reload working again.'
+  div.innerHTML = innerHtml
   div.style = style
+  div.addEventListener('click', function() { location.reload() })
+
   document.body.appendChild(div)
 }
